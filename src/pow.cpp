1e680bc530a39ccce97a08ead694af2b23b0eac1
blackjok3r <blackjok3r@gmail.com>
2018-10-07 10:26:07 +0800

    remove everything alright tried that didnt work I think

--- src/pow.cpp
+++ src/pow.cpp
@@ -146,7 +146,7 @@ bool CheckProofOfWork(int32_t height,uint8_t *pubkey33,uint256 hash,unsigned int
     memcpy(origpubkey33,pubkey33,33);
     memset(blocktimes,0,sizeof(blocktimes));
     tiptime = komodo_chainactive_timestamp();
-    bnTarget.SetCompact(KOMODO_MINDIFF_NBITS,&fNegative,&fOverflow);
+    bnTarget.SetCompact(nBits,&fNegative,&fOverflow);
     if ( height == 0 )
     {
         height = komodo_currentheight() + 1;
@@ -238,7 +238,7 @@ arith_uint256 GetBlockProof(const CBlockIndex& block)
     arith_uint256 bnTarget;
     bool fNegative;
     bool fOverflow;
-    bnTarget.SetCompact(KOMODO_MINDIFF_NBITS,&fNegative,&fOverflow);
+    bnTarget.SetCompact(block.nBits,&fNegative,&fOverflow);
     if (fNegative || fOverflow || bnTarget == 0)
         return 0;
     // We need to compute 2**256 / (bnTarget+1), but we can't represent 2**256
